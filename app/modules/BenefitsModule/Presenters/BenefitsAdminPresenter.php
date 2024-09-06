21847fa374b15fc4244308c5ed1c983d5e2599bd
David Kolembus <kolembus.business@gmail.com>
2024-09-07 00:46:15 +0200

    Benefit Edit form

--- app/modules/BenefitsModule/Presenters/BenefitsAdminPresenter.php
+++ app/modules/BenefitsModule/Presenters/BenefitsAdminPresenter.php
@@ -2,6 +2,7 @@
 
 namespace Crm\BenefitsModule\Presenters;
 use Crm\BenefitsModule\Repositories\BenefitsRepository;
+use Tomaj\Form\Renderer\BootstrapRenderer;
 
 class BenefitsAdminPresenter extends \Crm\AdminModule\Presenters\AdminPresenter
 {
@@ -24,6 +25,21 @@ class BenefitsAdminPresenter extends \Crm\AdminModule\Presenters\AdminPresenter
     public function renderEdit($id): void
     {
         $this->template->benefit = $this->benefitsRepository->find($id);
+    }
+
+    public function createComponentEditBenefitForm(): \Nette\Application\UI\Form
+    {
+        $form = new \Nette\Application\UI\Form();
+        $form->setRenderer(new BootstrapRenderer());
+
+        $form->addText('name', 'Name')->setDefaultValue($this->template->benefit->name);
+        $form->addText('code', 'Code')->setDefaultValue($this->template->benefit->code);
+        $form->addText('photo', 'Photo Url')->setDefaultValue($this->template->benefit->photo);
+        $form->addDateTime('start_date', 'Start Date')->setDefaultValue($this->template->benefit->start_date);
+        $form->addDateTime('end_date', 'End Date')->setDefaultValue($this->template->benefit->end_date);
+        $form->addSubmit('save', 'Save');
+
+        return $form;
     }
 
 }
