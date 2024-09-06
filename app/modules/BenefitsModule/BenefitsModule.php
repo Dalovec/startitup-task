215feb9313bad7c3b6faceea5ba434e885a29a34
David Kolembus <kolembus.business@gmail.com>
2024-09-06 10:28:30 +0200

    Get Base Admin Presenter and menu items down

--- app/modules/BenefitsModule/BenefitsModule.php
+++ app/modules/BenefitsModule/BenefitsModule.php
@@ -2,7 +2,26 @@
 
 namespace Crm\BenefitsModule;
 
+use Crm\ApplicationModule\Models\Menu\MenuContainerInterface;
+use Crm\ApplicationModule\Models\Menu\MenuItem;
+
 class BenefitsModule extends \Crm\ApplicationModule\CrmModule
 {
-    // register your extension points based on the documentation below
+    public function registerAdminMenuItems(MenuContainerInterface $menuContainer): void
+    {
+        $mainMenu = new MenuItem(
+            $this->translator->translate('Benefity'),
+            ':Benefits:BenefitsAdmin:default',
+            'fa fa-face-smile',
+            500,
+            true
+        );
+
+        $menuContainer->attachMenuItem($mainMenu);
+    }
+
+    public function registerRoutes(\Nette\Application\Routers\RouteList $router): void
+    {
+        $router->addRoute('/benefits', 'Benefits:BenefitsAdmin:default');
+    }
 }
---
+++ app/modules/BenefitsModule/Presenters/BenefitsAdminPresenter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Crm\BenefitsModule\Presenters;
+class BenefitsAdminPresenter extends \Crm\AdminModule\Presenters\AdminPresenter
+{
+    public function renderDefault()
+    {
+        // in Admin presenters even empty render methods are necessary
+    }
+}
