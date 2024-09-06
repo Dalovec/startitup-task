03f4faca8d68d3af899148da6825e16143064459
David Kolembus <kolembus.business@gmail.com>
2024-09-06 19:26:30 +0200

    Repository INIT

--- app/modules/BenefitsModule/Presenters/BenefitsAdminPresenter.php
+++ app/modules/BenefitsModule/Presenters/BenefitsAdminPresenter.php
@@ -1,8 +1,20 @@
 <?php
 
 namespace Crm\BenefitsModule\Presenters;
+use Crm\BenefitsModule\Repositories\BenefitsRepository;
+
 class BenefitsAdminPresenter extends \Crm\AdminModule\Presenters\AdminPresenter
 {
+    private BenefitsRepository $benefitsRepository;
+
+    public function __construct(
+        BenefitsRepository $benefitsRepository,
+    )
+    {
+        parent::__construct();
+        $this->benefitsRepository = $benefitsRepository;
+    }
+
     public function renderDefault()
     {
         // in Admin presenters even empty render methods are necessary
---
+++ app/modules/BenefitsModule/Repositories/BenefitsRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Crm\BenefitsModule\Repositories;
+
+use Crm\ApplicationModule\Models\Database\Repository;
+use Crm\ApplicationModule\Repositories\AuditLogRepository;
+use Nette\Database\Explorer;
+
+class BenefitsRepository extends Repository
+{
+    protected $tableName = 'benefits';
+    public function __construct(
+        Explorer           $database,
+        AuditLogRepository $auditLogRepository
+    )
+    {
+        parent::__construct($database);
+        $this->auditLogRepository = $auditLogRepository;
+    }
+
+    final public function all(): \Crm\ApplicationModule\Models\Database\Selection
+    {
+        return $this->getTable()->where('name IS NOT NULL');
+    }
+}
