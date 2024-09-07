b0fe1537637a16864e4a7cd094b1fe911b8a0ce0
David Kolembus <kolembus.business@gmail.com>
2024-09-07 17:58:34 +0200

    Widget for user detail

--- app/modules/BenefitsModule/Components/UserBenefitsListing/UserBenefitsListing.php
+++ app/modules/BenefitsModule/Components/UserBenefitsListing/UserBenefitsListing.php
@@ -3,6 +3,9 @@
 namespace Crm\BenefitsModule\Components\UserBenefitsListing;
 
 use Crm\ApplicationModule\Models\Widget\BaseLazyWidget;
+use Crm\ApplicationModule\Models\Widget\LazyWidgetManager;
+use Crm\BenefitsModule\Repositories\BenefitsRepository;
+use Crm\BenefitsModule\Repositories\UserBenefitsRepository;
 
 /**
  * This component fetches specific users benefits and renders them in a
@@ -13,20 +16,41 @@ use Crm\ApplicationModule\Models\Widget\BaseLazyWidget;
 class UserBenefitsListing extends BaseLazyWidget
 {
     private $templateName = 'demo.latte';
-
+    private $benefitsRepository;
+    private $userBenefitsRepository;
+    private $lazyWidgetManager;
+    public function __construct(
+        LazyWidgetManager $lazyWidgetManager,
+        BenefitsRepository $benefitsRepository,
+        UserBenefitsRepository $userBenefitsRepository,
+    ){
+        parent::__construct($lazyWidgetManager);
+        $this->lazyWidgetManager = $lazyWidgetManager;
+        $this->benefitsRepository = $benefitsRepository;
+        $this->userBenefitsRepository = $userBenefitsRepository;
+    }
     public function identifier()
     {
         return 'demowidget';
     }
 
-    public function header()
+    public function header($id = '')
     {
         return 'Benefits';
     }
 
-    public function render()
+    public function render($id)
     {
         $this->template->setFile(__DIR__ . DIRECTORY_SEPARATOR . $this->templateName);
+        if ($this->userBenefitsRepository->isClaimed($id)) {
+            $this->template->benefit = $this->benefitsRepository->find($this->userBenefitsRepository->isClaimed($id));
+        }
+        $this->template->userId = $id;
         $this->template->render();
+    }
+
+    public function handleUnclaim($id)
+    {
+        $this->userBenefitsRepository->resetUser($id);
     }
 }
